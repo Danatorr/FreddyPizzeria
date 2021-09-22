67ac56f3e7814c52d7d90a63d67f3f087d59f303
Torres <dan03torres@gmail.com>
2021-09-22 18:36:04 -0300

    Controller added

---
+++ FreddyPizzeria/FreddyPizzeria/Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using ContosoPizza.Services;
+using FreddyPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreddyPizzeria.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+
+        }
+
+        //Get (all)
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        //Get (by Id)
+    }
+}
