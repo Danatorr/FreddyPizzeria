1fe99498bb19d30c28e5233cd825c409b965666d
Torres <dan03torres@gmail.com>
2021-09-22 18:30:00 -0300

    Local Storage Update

---
+++ FreddyPizzeria/FreddyPizzeria/Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreddyPizzeria.Models
+{
+    public class Pizza
+    {
+        //Defining the attributes to the pizza
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+        //Thought about adding flavor and size, but it would be useless, because it would be already in the name of the pizza xD
+    }
+}
